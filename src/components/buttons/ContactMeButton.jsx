2d569fb23d556f5c4a1e4305f1085733e8945654
BudraHH <hariharabudra@gmail.com>
2024-10-30 14:13:06 +0530

    updated

--- src/components/buttons/ContactMeButton.jsx
+++ src/components/buttons/ContactMeButton.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import { HashLink as Link } from "react-router-hash-link"; // Import HashLink for smooth scrolling
 
 const ContactMeButton = () => {
